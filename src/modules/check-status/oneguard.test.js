e1e9d3f71def7dc0a62416750b0497b9732616ca
Frank Gutierrez <dearfrankg@gmail.com>
2020-06-04 13:21:46 -0700

    add oneguard unit tests

--- src/modules/check-status/oneguard.test.js
+++ src/modules/check-status/oneguard.test.js
@@ -6,128 +6,281 @@ const {
 const checkStatus = require("./check-status").checkStatus;
 const getOptions = require("../../utils/get-options").getOptions;
 
-describe("prco-check-status for oneguard", () => {
+describe("prco-check-status oneguard support", () => {
   let args;
 
-  beforeAll(() => {
-    rimraf.sync(__dirname + "/__downloads__/oneguard/a/b/*");
-    rimraf.sync(__dirname + "/__downloads__/oneguard/b o b/*");
-    process.env.wis_credentials = "foo,bar";
-    process.env.onegard_credentials = "foo,bar";
-  });
+  describe("test environment", () => {
+    beforeAll(() => {
+      rimraf.sync(__dirname + "/__downloads__/oneguard/test/a/b/*");
+      rimraf.sync(__dirname + "/__downloads__/oneguard/test/b o b/*");
+      process.env.wis_credentials = "foo,bar";
+      process.env.onegard_credentials = "foo,bar";
+    });
 
-  beforeEach(() => {
-    fetch.resetMocks();
-    args = ["node", "prco-check-status", "-e", "test", "-s", "oneguard", "15748,/oneguard/a/b/111"];
-  });
+    beforeEach(() => {
+      fetch.resetMocks();
+      args = [
+        "node",
+        "prco-check-status",
+        "-s",
+        "oneguard",
+        "-e",
+        "test",
+        "15748,/oneguard/test/a/b/111",
+      ];
+    });
 
-  describe("when passed a valid request", () => {
-    it("should return fomatted report with one entry", async () => {
-      const response = {
-        body: oneguardSoapResponseGood,
-        status: 222,
-      };
-      fetch.once(() => Promise.resolve(response));
+    describe("when passed a valid request", () => {
+      describe("using paths with spaces", () => {
+        it("should return fomatted report with one entry", async () => {
+          const response = {
+            body: oneguardSoapResponseGood,
+            status: 222,
+          };
+          fetch.once(() => Promise.resolve(response));
 
-      process.argv = [...args];
-      const options = await getOptions();
-      const responses = await checkStatus(options);
-      const report = responses[0].prco.finalReport;
+          process.argv = [...args.slice(0, -1), "15748,/oneguard/test/b o b/333"];
+          const options = await getOptions();
+          const responses = await checkStatus(options);
+          const report = responses[0].prco.finalReport;
 
-      expect(fetch.mock.calls.length).toEqual(1);
-      expect(report).toMatchSnapshot();
+          expect(fetch.mock.calls.length).toEqual(1);
+          expect(report).toMatchSnapshot();
+        });
+      });
+
+      describe("using paths without spaces", () => {
+        it("should return fomatted report with one entry", async () => {
+          const response = {
+            body: oneguardSoapResponseGood,
+            status: 222,
+          };
+          fetch.once(() => Promise.resolve(response));
+
+          process.argv = [...args];
+          const options = await getOptions();
+          const responses = await checkStatus(options);
+          const report = responses[0].prco.finalReport;
+
+          expect(fetch.mock.calls.length).toEqual(1);
+          expect(report).toMatchSnapshot();
+        });
+      });
+    });
+
+    describe("when passed multiple valid requests", () => {
+      it("should return fomatted report with mutiple entrys", async () => {
+        const response = {
+          body: oneguardSoapResponseGood,
+          status: 222,
+        };
+        fetch.once(() => Promise.resolve(response));
+        fetch.once(() => Promise.resolve(response));
+
+        process.argv = [...args, "15748,/oneguard/test/a/b/222"];
+        const options = await getOptions();
+        const responses = await checkStatus(options);
+        const report = responses[0].prco.finalReport;
+
+        expect(fetch.mock.calls.length).toEqual(2);
+        expect(report).toMatchSnapshot();
+      });
+    });
+
+    describe("when passed an invalid request", () => {
+      it("should return fomatted report with one message: Status unavailable", async () => {
+        const response = {
+          body: oneguardSoapResponseMissing,
+          status: 222,
+        };
+        fetch.once(() => Promise.resolve(response));
+
+        process.argv = [...args.slice(0, -1), "838317,/oneguard/test/a/b/555"];
+        const options = await getOptions();
+        const responses = await checkStatus(options);
+        const report = responses[0].prco.finalReport;
+
+        expect(fetch.mock.calls.length).toEqual(1);
+        expect(report).toMatchSnapshot();
+      });
+    });
+
+    describe("when passed multiple invalid requests", () => {
+      it("should return fomatted report with mutiple messages: Status unavailable", async () => {
+        const response = {
+          body: oneguardSoapResponseMissing,
+          status: 222,
+        };
+        fetch.once(() => Promise.resolve(response));
+        fetch.once(() => Promise.resolve(response));
+
+        process.argv = [
+          ...args.slice(0, -1),
+          "838317,/oneguard/test/a/b/666",
+          "838317,/oneguard/test/a/b/777",
+        ];
+        const options = await getOptions();
+        const responses = await checkStatus(options);
+        const report = responses[0].prco.finalReport;
+
+        expect(fetch.mock.calls.length).toEqual(2);
+        expect(report).toMatchSnapshot();
+      });
+    });
+
+    describe("when encountering a server error", () => {
+      it("should return fomatted report with message: Server error", async () => {
+        const errorResponse = {
+          body: oneguardSoapResponseGood,
+          status: 500,
+        };
+        fetch.once(() => Promise.resolve(errorResponse));
+
+        process.argv = [...args];
+        const options = await getOptions();
+        const responses = await checkStatus(options);
+        const report = responses[0].prco.finalReport;
+
+        expect(fetch.mock.calls.length).toEqual(1);
+        expect(report).toMatchSnapshot();
+      });
     });
   });
 
-  describe("when passed multiple valid requests", () => {
-    it("should return fomatted report with mutiple entrys", async () => {
-      const response = {
-        body: oneguardSoapResponseGood,
-        status: 222,
-      };
-      fetch.once(() => Promise.resolve(response));
-      fetch.once(() => Promise.resolve(response));
-
-      process.argv = [...args, "15748,/oneguard/a/b/222"];
-      const options = await getOptions();
-      const responses = await checkStatus(options);
-      const report = responses[0].prco.finalReport;
-
-      expect(fetch.mock.calls.length).toEqual(2);
-      expect(report).toMatchSnapshot();
+  describe("production environment", () => {
+    beforeAll(() => {
+      rimraf.sync(__dirname + "/__downloads__/oneguard/production/a/b/*");
+      rimraf.sync(__dirname + "/__downloads__/oneguard/production/b o b/*");
+      process.env.wis_credentials = "foo,bar";
+      process.env.onegard_credentials = "foo,bar";
     });
-  });
 
-  describe("when passed a valid request and paths that have spaces", () => {
-    it("should return fomatted report with one entry", async () => {
-      const response = {
-        body: oneguardSoapResponseGood,
-        status: 222,
-      };
-      fetch.once(() => Promise.resolve(response));
-
-      process.argv = [...args.slice(0, -1), "15748,/oneguard/b o b/333"];
-      const options = await getOptions();
-      const responses = await checkStatus(options);
-      const report = responses[0].prco.finalReport;
-
-      expect(fetch.mock.calls.length).toEqual(1);
-      expect(report).toMatchSnapshot();
+    beforeEach(() => {
+      fetch.resetMocks();
+      args = [
+        "node",
+        "prco-check-status",
+        "-s",
+        "oneguard",
+        "-e",
+        "production",
+        "197961,/oneguard/production/a/b/111",
+      ];
     });
-  });
 
-  describe("when passed an invalid request", () => {
-    it("should return fomatted report with one message: Status unavailable", async () => {
-      const response = {
-        body: oneguardSoapResponseMissing,
-        status: 222,
-      };
-      fetch.once(() => Promise.resolve(response));
+    describe("when passed a valid request", () => {
+      describe("using paths with spaces", () => {
+        it("should return fomatted report with one entry", async () => {
+          const response = {
+            body: oneguardSoapResponseGood,
+            status: 222,
+          };
+          fetch.once(() => Promise.resolve(response));
 
-      process.argv = [...args.slice(0, -1), "838317,/oneguard/a/b/555"];
-      const options = await getOptions();
-      const responses = await checkStatus(options);
-      const report = responses[0].prco.finalReport;
+          process.argv = [...args.slice(0, -1), "197961,/oneguard/production/b o b/333"];
+          const options = await getOptions();
+          const responses = await checkStatus(options);
+          const report = responses[0].prco.finalReport;
 
-      expect(fetch.mock.calls.length).toEqual(1);
-      expect(report).toMatchSnapshot();
+          expect(fetch.mock.calls.length).toEqual(1);
+          expect(report).toMatchSnapshot();
+        });
+      });
+      describe("using paths without spaces", () => {
+        it("should return fomatted report with one entry", async () => {
+          const response = {
+            body: oneguardSoapResponseGood,
+            status: 222,
+          };
+          fetch.once(() => Promise.resolve(response));
+
+          process.argv = [...args];
+          const options = await getOptions();
+          const responses = await checkStatus(options);
+          const report = responses[0].prco.finalReport;
+
+          expect(fetch.mock.calls.length).toEqual(1);
+          expect(report).toMatchSnapshot();
+        });
+      });
     });
-  });
 
-  describe("when passed multiple invalid requests", () => {
-    it("should return fomatted report with mutiple messages: Status unavailable", async () => {
-      const response = {
-        body: oneguardSoapResponseMissing,
-        status: 222,
-      };
-      fetch.once(() => Promise.resolve(response));
-      fetch.once(() => Promise.resolve(response));
+    describe("when passed multiple valid requests", () => {
+      it("should return fomatted report with mutiple entrys", async () => {
+        const response = {
+          body: oneguardSoapResponseGood,
+          status: 222,
+        };
+        fetch.once(() => Promise.resolve(response));
+        fetch.once(() => Promise.resolve(response));
 
-      process.argv = [...args.slice(0, -1), "838317,/a/b/666", "838317,/a/b/777"];
-      const options = await getOptions();
-      const responses = await checkStatus(options);
-      const report = responses[0].prco.finalReport;
+        process.argv = [...args, "197961,/oneguard/production/a/b/222"];
+        const options = await getOptions();
+        const responses = await checkStatus(options);
+        const report = responses[0].prco.finalReport;
 
-      expect(fetch.mock.calls.length).toEqual(2);
-      expect(report).toMatchSnapshot();
+        expect(fetch.mock.calls.length).toEqual(2);
+        expect(report).toMatchSnapshot();
+      });
     });
-  });
 
-  describe("when encountering a server error", () => {
-    it("should return fomatted report with message: Server error", async () => {
-      const errorResponse = {
-        body: oneguardSoapResponseGood,
-        status: 500,
-      };
-      fetch.once(() => Promise.resolve(errorResponse));
+    describe("when passed an invalid request", () => {
+      it("should return fomatted report with one message: Status unavailable", async () => {
+        const response = {
+          body: oneguardSoapResponseMissing,
+          status: 222,
+        };
+        fetch.once(() => Promise.resolve(response));
 
-      process.argv = [...args];
-      const options = await getOptions();
-      const responses = await checkStatus(options);
-      const report = responses[0].prco.finalReport;
+        process.argv = [...args.slice(0, -1), "197961,/oneguard/production/a/b/555"];
+        const options = await getOptions();
+        const responses = await checkStatus(options);
+        const report = responses[0].prco.finalReport;
 
-      expect(fetch.mock.calls.length).toEqual(1);
-      expect(report).toMatchSnapshot();
+        expect(fetch.mock.calls.length).toEqual(1);
+        expect(report).toMatchSnapshot();
+      });
+    });
+
+    describe("when passed multiple invalid requests", () => {
+      it("should return fomatted report with mutiple messages: Status unavailable", async () => {
+        const response = {
+          body: oneguardSoapResponseMissing,
+          status: 222,
+        };
+        fetch.once(() => Promise.resolve(response));
+        fetch.once(() => Promise.resolve(response));
+
+        process.argv = [
+          ...args.slice(0, -1),
+          "197961,/oneguard/production/a/b/666",
+          "196361,/oneguard/production/a/b/777",
+        ];
+        const options = await getOptions();
+        const responses = await checkStatus(options);
+        const report = responses[0].prco.finalReport;
+
+        expect(fetch.mock.calls.length).toEqual(2);
+        expect(report).toMatchSnapshot();
+      });
+    });
+
+    describe("when encountering a server error", () => {
+      it("should return fomatted report with message: Server error", async () => {
+        const errorResponse = {
+          body: oneguardSoapResponseGood,
+          status: 500,
+        };
+        fetch.once(() => Promise.resolve(errorResponse));
+
+        process.argv = [...args];
+        const options = await getOptions();
+        const responses = await checkStatus(options);
+        const report = responses[0].prco.finalReport;
+
+        expect(fetch.mock.calls.length).toEqual(1);
+        expect(report).toMatchSnapshot();
+      });
     });
   });
 });
