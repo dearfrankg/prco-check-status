05f55767b8b7db5a08bf6d62d28d71cffd048ed8
Frank Gutierrez <dearfrankg@gmail.com>
2020-06-05 01:05:33 -0700

    add updated wis unit tests

--- src/modules/check-status/wis.test.js
+++ src/modules/check-status/wis.test.js
@@ -3,23 +3,41 @@ const { wisSoapResponseGood, wisSoapResponseMissing } = require("./__fixtures__/
 const checkStatus = require("./check-status").checkStatus;
 const getOptions = require("../../utils/get-options").getOptions;
 
-describe("prco-check-status for wis", () => {
+describe("prco-check-status wis support", () => {
   let args;
 
-  beforeAll(() => {
-    rimraf.sync(__dirname + "/__downloads__/wis/a/b/*");
-    rimraf.sync(__dirname + "/__downloads__/wis/b o b/*");
-    process.env.wis_credentials = "foo,bar";
-    process.env.onegard_credentials = "foo,bar";
-  });
+  describe("test environment", () => {
+    beforeAll(() => {
+      rimraf.sync(__dirname + "/__downloads__/wis/test/a/b/*");
+      rimraf.sync(__dirname + "/__downloads__/wis/test/b o b/*");
+      process.env.wis_credentials = "foo,bar";
+      process.env.onegard_credentials = "foo,bar";
+    });
 
-  beforeEach(() => {
-    fetch.resetMocks();
-    args = ["node", "prco-check-status", "-e", "production", "-s", "wis", "758317,/wis/a/b/111"];
-  });
+    beforeEach(() => {
+      fetch.resetMocks();
+      args = ["node", "prco-check-status", "-s", "wis", "-e", "test", "758317,/wis/test/a/b/111"];
+    });
 
-  describe("production environment", () => {
     describe("when passed a valid request", () => {
+      describe("using paths with spaces", () => {
+        it("should return fomatted report with one entry", async () => {
+          const response = {
+            body: wisSoapResponseGood,
+            status: 222,
+          };
+          fetch.once(() => Promise.resolve(response));
+
+          process.argv = [...args.slice(0, -1), "758317,/wis/test/b o b/333"];
+          const options = await getOptions();
+          const responses = await checkStatus(options);
+          const report = responses[0].prco.finalReport;
+
+          expect(fetch.mock.calls.length).toEqual(1);
+          expect(report).toMatchSnapshot();
+        });
+      });
+
       describe("using paths without spaces", () => {
         it("should return fomatted report with one entry", async () => {
           const response = {
@@ -29,23 +47,6 @@ describe("prco-check-status for wis", () => {
           fetch.once(() => Promise.resolve(response));
 
           process.argv = [...args];
-          const options = await getOptions();
-          const responses = await checkStatus(options);
-          const report = responses[0].prco.finalReport;
-
-          expect(fetch.mock.calls.length).toEqual(1);
-          expect(report).toMatchSnapshot();
-        });
-      });
-      describe("using paths with spaces", () => {
-        it("should return fomatted report with one entry", async () => {
-          const response = {
-            body: wisSoapResponseGood,
-            status: 222,
-          };
-          fetch.once(() => Promise.resolve(response));
-
-          process.argv = [...args.slice(0, -1), "758317,/wis/b o b/333"];
           const options = await getOptions();
           const responses = await checkStatus(options);
           const report = responses[0].prco.finalReport;
@@ -65,7 +66,7 @@ describe("prco-check-status for wis", () => {
         fetch.once(() => Promise.resolve(response));
         fetch.once(() => Promise.resolve(response));
 
-        process.argv = [...args, "758317,/wis/a/b/222"];
+        process.argv = [...args, "758317,/wis/test/a/b/222"];
         const options = await getOptions();
         const responses = await checkStatus(options);
         const report = responses[0].prco.finalReport;
@@ -83,7 +84,7 @@ describe("prco-check-status for wis", () => {
         };
         fetch.once(() => Promise.resolve(response));
 
-        process.argv = [...args.slice(0, -1), "838317,/wis/a/b/555"];
+        process.argv = [...args.slice(0, -1), "838317,/wis/test/a/b/555"];
         const options = await getOptions();
         const responses = await checkStatus(options);
         const report = responses[0].prco.finalReport;
@@ -102,7 +103,149 @@ describe("prco-check-status for wis", () => {
         fetch.once(() => Promise.resolve(response));
         fetch.once(() => Promise.resolve(response));
 
-        process.argv = [...args.slice(0, -1), "838317,/a/b/666", "838317,/a/b/777"];
+        process.argv = [
+          ...args.slice(0, -1),
+          "838317,/wis/test/a/b/666",
+          "838317,/wis/test/a/b/777",
+        ];
+        const options = await getOptions();
+        const responses = await checkStatus(options);
+        const report = responses[0].prco.finalReport;
+
+        expect(fetch.mock.calls.length).toEqual(2);
+        expect(report).toMatchSnapshot();
+      });
+    });
+
+    describe("when encountering a server error", () => {
+      it("should return fomatted report with message: Server error", async () => {
+        const errorResponse = {
+          body: wisSoapResponseGood,
+          status: 500,
+        };
+        fetch.once(() => Promise.resolve(errorResponse));
+
+        process.argv = [...args];
+        const options = await getOptions();
+        const responses = await checkStatus(options);
+        const report = responses[0].prco.finalReport;
+
+        expect(fetch.mock.calls.length).toEqual(1);
+        expect(report).toMatchSnapshot();
+      });
+    });
+  });
+
+  describe("production environment", () => {
+    beforeAll(() => {
+      rimraf.sync(__dirname + "/__downloads__/wis/production/a/b/*");
+      rimraf.sync(__dirname + "/__downloads__/wis/production/b o b/*");
+      process.env.wis_credentials = "foo,bar";
+      process.env.onegard_credentials = "foo,bar";
+    });
+
+    beforeEach(() => {
+      fetch.resetMocks();
+      args = [
+        "node",
+        "prco-check-status",
+        "-s",
+        "wis",
+        "-e",
+        "production",
+        "758317,/wis/production/a/b/111",
+      ];
+    });
+
+    describe("when passed a valid request", () => {
+      describe("using paths with spaces", () => {
+        it("should return fomatted report with one entry", async () => {
+          const response = {
+            body: wisSoapResponseGood,
+            status: 222,
+          };
+          fetch.once(() => Promise.resolve(response));
+
+          process.argv = [...args.slice(0, -1), "758317,/wis/production/b o b/333"];
+          const options = await getOptions();
+          const responses = await checkStatus(options);
+          const report = responses[0].prco.finalReport;
+
+          expect(fetch.mock.calls.length).toEqual(1);
+          expect(report).toMatchSnapshot();
+        });
+      });
+
+      describe("using paths without spaces", () => {
+        it("should return fomatted report with one entry", async () => {
+          const response = {
+            body: wisSoapResponseGood,
+            status: 222,
+          };
+          fetch.once(() => Promise.resolve(response));
+
+          process.argv = [...args];
+          const options = await getOptions();
+          const responses = await checkStatus(options);
+          const report = responses[0].prco.finalReport;
+
+          expect(fetch.mock.calls.length).toEqual(1);
+          expect(report).toMatchSnapshot();
+        });
+      });
+    });
+
+    describe("when passed multiple valid requests", () => {
+      it("should return fomatted report with mutiple entrys", async () => {
+        const response = {
+          body: wisSoapResponseGood,
+          status: 222,
+        };
+        fetch.once(() => Promise.resolve(response));
+        fetch.once(() => Promise.resolve(response));
+
+        process.argv = [...args, "758317,/wis/production/a/b/222"];
+        const options = await getOptions();
+        const responses = await checkStatus(options);
+        const report = responses[0].prco.finalReport;
+
+        expect(fetch.mock.calls.length).toEqual(2);
+        expect(report).toMatchSnapshot();
+      });
+    });
+
+    describe("when passed an invalid request", () => {
+      it("should return fomatted report with one message: Status unavailable", async () => {
+        const response = {
+          body: wisSoapResponseMissing,
+          status: 222,
+        };
+        fetch.once(() => Promise.resolve(response));
+
+        process.argv = [...args.slice(0, -1), "838317,/wis/production/a/b/555"];
+        const options = await getOptions();
+        const responses = await checkStatus(options);
+        const report = responses[0].prco.finalReport;
+
+        expect(fetch.mock.calls.length).toEqual(1);
+        expect(report).toMatchSnapshot();
+      });
+    });
+
+    describe("when passed multiple invalid requests", () => {
+      it("should return fomatted report with mutiple messages: Status unavailable", async () => {
+        const response = {
+          body: wisSoapResponseMissing,
+          status: 222,
+        };
+        fetch.once(() => Promise.resolve(response));
+        fetch.once(() => Promise.resolve(response));
+
+        process.argv = [
+          ...args.slice(0, -1),
+          "838317,/wis/production//a/b/666",
+          "838317,/wis/production//a/b/777",
+        ];
         const options = await getOptions();
         const responses = await checkStatus(options);
         const report = responses[0].prco.finalReport;
